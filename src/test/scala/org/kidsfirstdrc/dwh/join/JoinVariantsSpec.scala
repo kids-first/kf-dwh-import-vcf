599a5cd7b69738f16f62cf727acfd061d30afc4f
jecos <jcostanza@ferlab.bio>
2020-10-26 14:34:04 -0400

    Rename class

--- src/test/scala/org/kidsfirstdrc/dwh/join/JoinVariantsSpec.scala
+++ src/test/scala/org/kidsfirstdrc/dwh/join/JoinVariantsSpec.scala
@@ -8,7 +8,7 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 
-class JoinDemoVariantsSpec extends AnyFlatSpec with GivenWhenThen with WithSparkSession with Matchers {
+class JoinVariantsSpec extends AnyFlatSpec with GivenWhenThen with WithSparkSession with Matchers {
 
   import spark.implicits._
 
