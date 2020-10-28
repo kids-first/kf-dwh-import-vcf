28e471f92c2e44fd8427077a382eb271df19de6b
jecos <jcostanza@ferlab.bio>
2020-10-28 13:53:42 -0400

    Fix job inport bash script after migration

--- bin/run_emr_job_import.sh
+++ bin/run_emr_job_import.sh
@@ -7,12 +7,41 @@ instance_type=${5:-"m5d.8xlarge"}
 number_instance=${6:-"30"}
 study_id_lc=$(echo "$study_id" | tr '[:upper:]' '[:lower:]' | tr '_' '-')
 biospecimen_id_column=${7:-"biospecimen_id"}
+steps=$(cat <<EOF
+[
+  {
+    "Args": [
+      "spark-submit",
+      "--packages",
+      "io.projectglow:glow_2.11:0.5.0",
+      "--exclude-packages",
+      "org.apache.httpcomponents:httpcore,org.apache.httpcomponents:httpclient",
+      "--deploy-mode",
+      "client",
+      "--class",
+      "org.kidsfirstdrc.dwh.vcf.ImportVcf",
+      "s3a://kf-strides-variant-parquet-prd/jobs/kf-dwh-import-vcf.jar"
+      "${study_id}",
+      "${release_id}",
+      "${input_vcf}",
+      "s3a://kf-strides-variant-parquet-prd",
+      "${job}",
+      "${biospecimen_id_column}"
+    ],
+    "Type": "CUSTOM_JAR",
+    "ActionOnFailure": "TERMINATE_CLUSTER",
+    "Jar": "command-runner.jar",
+    "Properties": "",
+    "Name": "Spark application"
+  }
+]
+EOF
+)
 
-steps="[{\"Args\":[\"spark-submit\",\"--packages\",\"io.projectglow:glow_2.11:0.3.0\", \"--exclude-packages\", \"org.apache.httpcomponents:httpcore,org.apache.httpcomponents:httpclient\",\"--deploy-mode\",\"client\",\"--class\",\"org.kidsfirstdrc.dwh.vcf.ImportVcf\",\"s3a://kf-variant-parquet-prd/jobs/kf-dwh-import-vcf.jar\",\"${study_id}\",\"${release_id}\",\"${input_vcf}\",\"s3a://kf-variant-parquet-prd\", \"${job}\", \"${biospecimen_id_column}\"],\"Type\":\"CUSTOM_JAR\",\"ActionOnFailure\":\"TERMINATE_CLUSTER\",\"Jar\":\"command-runner.jar\",\"Properties\":\"\",\"Name\":\"Spark application\"}]"
 instance_groups="[{\"InstanceCount\":${number_instance},\"InstanceGroupType\":\"CORE\",\"InstanceType\":\"${instance_type}\",\"Name\":\"Core - 2\"},{\"InstanceCount\":1,\"EbsConfiguration\":{\"EbsBlockDeviceConfigs\":[{\"VolumeSpecification\":{\"SizeInGB\":32,\"VolumeType\":\"gp2\"},\"VolumesPerInstance\":2}]},\"InstanceGroupType\":\"MASTER\",\"InstanceType\":\"m5.xlarge\",\"Name\":\"Master - 1\"}]"
 
 aws emr create-cluster --applications Name=Hadoop Name=Spark \
---ec2-attributes '{"KeyName":"flintrock","AdditionalSlaveSecurityGroups":["sg-059bf5fe80ff903be"],"InstanceProfile":"EMR_EC2_DefaultRole","ServiceAccessSecurityGroup":"sg-0487486fd3d67c14e","SubnetId":"subnet-a756a3ed","EmrManagedSlaveSecurityGroup":"sg-0807b9c40bb37be85","EmrManagedMasterSecurityGroup":"sg-012f30e67b51b6f4d","AdditionalMasterSecurityGroups":["sg-059bf5fe80ff903be"]}' \
+--ec2-attributes '{"KeyName":"flintrock","InstanceProfile":"EMR_EC2_DefaultRole","SubnetId":"subnet-031b7ef17a032fc3b","EmrManagedSlaveSecurityGroup":"sg-0d04e7c3ff5f36538","EmrManagedMasterSecurityGroup":"sg-0abad24e2a3e5e279"}' \
 --service-role EMR_DefaultRole \
 --enable-debugging \
 --release-label emr-5.29.0 \
