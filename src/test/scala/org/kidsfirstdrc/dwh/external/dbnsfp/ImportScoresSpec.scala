2307e778e07d420033e8e3b29e0aff1470892771
Christophe Botek <cbotek@ferlab.bio>
2020-12-23 09:23:32 -0500

    feat #2881 variant index now has scores

---
+++ src/test/scala/org/kidsfirstdrc/dwh/external/dbnsfp/ImportScoresSpec.scala
@@ -0,0 +1,23 @@
+package org.kidsfirstdrc.dwh.external.dbnsfp
+
+import org.kidsfirstdrc.dwh.testutils.WithSparkSession
+import org.kidsfirstdrc.dwh.testutils.external.ImportScores._
+import org.scalatest.GivenWhenThen
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+
+class ImportScoresSpec extends AnyFlatSpec with GivenWhenThen with WithSparkSession with Matchers {
+
+  import spark.implicits._
+
+  "ImportScores" should "transform data into expected format" in {
+
+    val inputDf = Seq(Input()).toDF()
+    val outputDf = ImportScores.transform(inputDf)
+
+    outputDf.as[Output].collect() should contain theSameElementsAs Seq(Output())
+
+  }
+
+}
