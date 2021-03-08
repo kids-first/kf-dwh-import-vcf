e66a1e9343507dde1620548804041df63d87fa3f
botekchristophe <botekchristophe@gmail.com>
2021-03-08 11:21:01 -0500

    created genes list in variant index

--- src/main/scala/org/kidsfirstdrc/dwh/external/package.scala
+++ src/main/scala/org/kidsfirstdrc/dwh/external/package.scala
@@ -3,7 +3,9 @@ package org.kidsfirstdrc.dwh
 package object external {
 
   object Dataservice {
-    val ALL_TABLES = Set("participants", "biospecimens", "biospecimens_diagnoses", "outcomes", "investigators", "genomic_files", "family_relationships", "families", "diagnoses", "sequencing_experiments", "studies", "outcomes", "study_files", "phenotypes", "genomic_files")
+    val ALL_TABLES = Set("participants", "biospecimens", "biospecimens_diagnoses", "outcomes", "investigators", "genomic_files",
+      "family_relationships", "families", "diagnoses", "sequencing_experiments", "studies", "outcomes", "study_files",
+      "phenotypes", "genomic_files")
   }
 
 }
