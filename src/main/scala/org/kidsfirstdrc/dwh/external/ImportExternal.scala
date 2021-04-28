190d2c73f5d7f4f214fc479a1235a614310056aa
cbotek <botekchristophe@gmail.com>
2021-04-28 15:33:42 -0400

    bump datalake-core lib to 0.0.10

--- src/main/scala/org/kidsfirstdrc/dwh/external/ImportExternal.scala
+++ src/main/scala/org/kidsfirstdrc/dwh/external/ImportExternal.scala
@@ -1,6 +1,6 @@
 package org.kidsfirstdrc.dwh.external
 
-import bio.ferlab.datalake.core.config.{Configuration, StorageConf}
+import bio.ferlab.datalake.core.config.{Configuration, ConfigurationLoader}
 import org.apache.spark.sql.SparkSession
 import org.kidsfirstdrc.dwh.external.clinvar.ImportClinVarJob
 import org.kidsfirstdrc.dwh.external.dbnsfp.{ImportAnnovarScores, ImportRaw, ImportScores}
@@ -9,16 +9,16 @@ import org.kidsfirstdrc.dwh.external.orphanet.ImportOrphanetGeneSet
 
 object ImportExternal extends App {
 
-  val Array(jobType, bucket) = args
+  val Array(jobType, config) = args
 
   implicit val spark: SparkSession = SparkSession.builder
     .config("hive.metastore.client.factory.class", "com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory")
     .enableHiveSupport()
     .appName("Import").getOrCreate()
 
-  implicit val conf: Configuration = Configuration(List(StorageConf("kf-strides-variant", bucket)))
+  //implicit val conf: Configuration = Configuration(List(StorageConf("kf-strides-variant", bucket)))
 
-  //implicit val conf: Configuration = ConfigurationLoader.loadFromResources(config)
+  implicit val conf: Configuration = ConfigurationLoader.loadFromResources(config)
 
   jobType.toLowerCase match {
     case "1000genomes"     => new Import1k().run()
