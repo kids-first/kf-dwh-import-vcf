ba4800f742a276d52deab5301587bda31f0f0152
Celine Pelletier <cpelletier@ferlab.bio>
2021-05-27 17:15:59 -0400

    feat: #3295 reformat new files

--- src/main/scala/org/kidsfirstdrc/dwh/external/gnomad/ImportGnomadV311Job.scala
+++ src/main/scala/org/kidsfirstdrc/dwh/external/gnomad/ImportGnomadV311Job.scala
@@ -2,16 +2,15 @@ package org.kidsfirstdrc.dwh.external.gnomad
 
 import bio.ferlab.datalake.spark3.config.Configuration
 import org.apache.spark.sql.functions.col
-import org.apache.spark.sql.{Column, DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.kidsfirstdrc.dwh.conf.Catalog.Public
 import org.kidsfirstdrc.dwh.conf.Catalog.Raw.gnomad_genomes_3_1_1
 import org.kidsfirstdrc.dwh.jobs.StandardETL
-import org.kidsfirstdrc.dwh.utils.SparkUtils.escapeInfoAndLowercase
 import org.kidsfirstdrc.dwh.utils.SparkUtils.columns._
-import org.kidsfirstdrc.dwh.utils.SparkUtils.vcf
+import org.kidsfirstdrc.dwh.utils.SparkUtils.{escapeInfoAndLowercase, vcf}
 
 class ImportGnomadV311Job(implicit conf: Configuration)
-  extends StandardETL(Public.gnomad_genomes_3_1_1)(conf) {
+    extends StandardETL(Public.gnomad_genomes_3_1_1)(conf) {
 
   override def extract()(implicit spark: SparkSession): Map[String, DataFrame] = {
     Map(gnomad_genomes_3_1_1.id -> vcf(gnomad_genomes_3_1_1.location))
@@ -29,12 +28,15 @@ class ImportGnomadV311Job(implicit conf: Configuration)
           alternate +:
           (col("qual") as "qual") +:
           name +:
-          escapeInfoAndLowercase(df): _*)
+          escapeInfoAndLowercase(df): _*
+      )
   }
 
   override def load(data: DataFrame)(implicit spark: SparkSession): DataFrame = {
-    super.load(data
-      .repartition(col("chromosome"))
-      .sortWithinPartitions("start"))
+    super.load(
+      data
+        .repartition(col("chromosome"))
+        .sortWithinPartitions("start")
+    )
   }
 }
--- src/test/scala/org/kidsfirstdrc/dwh/external/gnomad/ImportGnomadV311Spec.scala
+++ src/test/scala/org/kidsfirstdrc/dwh/external/gnomad/ImportGnomadV311Spec.scala
@@ -8,24 +8,21 @@ import org.scalatest.GivenWhenThen
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class ImportGnomadV311Spec extends AnyFlatSpec with GivenWhenThen with WithSparkSession with Matchers {
+class ImportGnomadV311Spec
+    extends AnyFlatSpec with GivenWhenThen with WithSparkSession with Matchers {
 
   import spark.implicits._
 
   implicit val conf: Configuration =
     Configuration(
-      List(StorageConf(
-        "kf-strides-variant",
-        getClass.getClassLoader.getResource(".").getFile)))
-
+      List(StorageConf("kf-strides-variant", getClass.getClassLoader.getResource(".").getFile))
+    )
 
   "transform" should "transform genomes from gnomad" in {
-
-    val inputData = Map(Raw.gnomad_genomes_3_1_1.id -> Seq(GnomadV311Input()).toDF())
-    val resultDF = new ImportGnomadV311Job().transform(inputData)
+    val inputData      = Map(Raw.gnomad_genomes_3_1_1.id -> Seq(GnomadV311Input()).toDF())
+    val resultDF       = new ImportGnomadV311Job().transform(inputData)
     val expectedResult = GnomadV311Output()
     resultDF.as[GnomadV311Output].collect().head shouldBe expectedResult
-
   }
 
 }
