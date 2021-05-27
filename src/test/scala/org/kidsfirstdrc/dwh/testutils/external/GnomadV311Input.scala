82747688d478b002d8b15564b84fd4c05096b210
Celine Pelletier <cpelletier@ferlab.bio>
2021-05-27 17:15:59 -0400

    feat: #3295 reformat new files

--- src/test/scala/org/kidsfirstdrc/dwh/testutils/external/GnomadV311Input.scala
+++ src/test/scala/org/kidsfirstdrc/dwh/testutils/external/GnomadV311Input.scala
@@ -1,18 +1,18 @@
 package org.kidsfirstdrc.dwh.testutils.external
 
 case class GnomadV311Input(
-                            contigName: String = "chr1",
-                            start: Long = 999,
-                            end: Long = 1009,
-                            referenceAllele: String = "A",
-                            alternateAlleles: List[String] = List("C"),
-                            qual: Double = 0.5,
-                            names: List[String] = List("BRAF"),
-                            INFO_AC: List[Long] = List(1),
-                            INFO_AC_raw: List[Long] = List(2),
-                            INFO_AF: List[BigDecimal] = List(3),
-                            INFO_AF_raw: List[BigDecimal] = List(4),
-                            INFO_AN: Long = 5,
-                            INFO_AN_raw: Long = 6,
-                            INFO_nhomalt_raw: Long = 7
-                          )
+    contigName: String = "chr1",
+    start: Long = 999,
+    end: Long = 1009,
+    referenceAllele: String = "A",
+    alternateAlleles: List[String] = List("C"),
+    qual: Double = 0.5,
+    names: List[String] = List("BRAF"),
+    INFO_AC: List[Long] = List(1),
+    INFO_AC_raw: List[Long] = List(2),
+    INFO_AF: List[BigDecimal] = List(3),
+    INFO_AF_raw: List[BigDecimal] = List(4),
+    INFO_AN: Long = 5,
+    INFO_AN_raw: Long = 6,
+    INFO_nhomalt_raw: Long = 7
+)
