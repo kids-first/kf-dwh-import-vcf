10352a38ed609a2673382bc766c3cf94ad658844
Celine Pelletier <cpelletier@ferlab.bio>
2021-05-28 14:18:17 -0400

    feat: #3295 flatten list in info fields

--- src/test/scala/org/kidsfirstdrc/dwh/testutils/external/GnomadV311Input.scala
+++ src/test/scala/org/kidsfirstdrc/dwh/testutils/external/GnomadV311Input.scala
@@ -10,9 +10,10 @@ case class GnomadV311Input(
     names: List[String] = List("BRAF"),
     INFO_AC: List[Long] = List(1),
     INFO_AC_raw: List[Long] = List(2),
-    INFO_AF: List[BigDecimal] = List(3),
-    INFO_AF_raw: List[BigDecimal] = List(4),
+    INFO_AF: List[BigDecimal] = List(BigDecimal(3.3)),
+    INFO_AF_raw: List[BigDecimal] = List(BigDecimal(4.4)),
     INFO_AN: Long = 5,
     INFO_AN_raw: Long = 6,
-    INFO_nhomalt_raw: Long = 7
+    INFO_nhomalt: List[Long] = List(7),
+    INFO_nhomalt_raw: List[Long] = List(8)
 )
