940c21e203c9fdde4cfa7bc0f2ce08d053541247
Celine Pelletier <cpelletier@ferlab.bio>
2021-05-31 17:28:38 -0400

    feat: #3295 review comments

--- src/test/scala/org/kidsfirstdrc/dwh/testutils/external/GnomadV311Input.scala
+++ src/test/scala/org/kidsfirstdrc/dwh/testutils/external/GnomadV311Input.scala
@@ -1,19 +1,19 @@
 package org.kidsfirstdrc.dwh.testutils.external
 
 case class GnomadV311Input(
-    contigName: String = "chr1",
-    start: Long = 999,
-    end: Long = 1009,
-    referenceAllele: String = "A",
-    alternateAlleles: List[String] = List("C"),
+    contigName: String = "chr2",
+    start: Long = 165310405,
+    end: Long = 165310406,
+    referenceAllele: String = "G",
+    alternateAlleles: List[String] = List("A"),
     qual: Double = 0.5,
     names: List[String] = List("BRAF"),
-    INFO_AC: List[Long] = List(1),
-    INFO_AC_raw: List[Long] = List(2),
-    INFO_AF: List[BigDecimal] = List(BigDecimal(3.3)),
-    INFO_AF_raw: List[BigDecimal] = List(BigDecimal(4.4)),
-    INFO_AN: Long = 5,
-    INFO_AN_raw: Long = 6,
-    INFO_nhomalt: List[Long] = List(7),
-    INFO_nhomalt_raw: List[Long] = List(8)
+    INFO_AC: List[Long] = List(10),
+    INFO_AC_raw: List[Long] = List(11),
+    INFO_AF: List[Double] = List(0.5),
+    INFO_AF_raw: List[Double] = List(0.6),
+    INFO_AN: Long = 20,
+    INFO_AN_raw: Long = 21,
+    INFO_nhomalt: List[Long] = List(10),
+    INFO_nhomalt_raw: List[Long] = List(11)
 )
--- src/test/scala/org/kidsfirstdrc/dwh/testutils/external/GnomadV311Output.scala
+++ src/test/scala/org/kidsfirstdrc/dwh/testutils/external/GnomadV311Output.scala
@@ -1,19 +1,19 @@
 package org.kidsfirstdrc.dwh.testutils.external
 
 case class GnomadV311Output(
-    chromosome: String = "1",
-    start: Long = 1000,
-    end: Long = 1010,
-    reference: String = "A",
-    alternate: String = "C",
+    chromosome: String = "2",
+    start: Long = 165310406,
+    end: Long = 165310407,
+    reference: String = "G",
+    alternate: String = "A",
+    ac: Long = 10,
+    ac_raw: Long = 11,
+    an: Long = 20,
+    an_raw: Long = 21,
+    af: Double = 0.5,
+    af_raw: Double = 0.6,
+    nhomalt: Long = 10,
+    nhomalt_raw: Long = 11,
     qual: Double = 0.5,
-    name: String = "BRAF",
-    ac: Long = 1,
-    ac_raw: Long = 2,
-    af: BigDecimal = BigDecimal(3.3),
-    af_raw: BigDecimal = BigDecimal(4.4),
-    an: Long = 5,
-    an_raw: Long = 6,
-    nhomalt: Long = 7,
-    nhomalt_raw: Long = 8
+    name: String = "BRAF"
 )
