1d35b90a9e687dc2118d62c720a5e5725917c80d
cbotek <botekchristophe@gmail.com>
2021-10-07 16:58:24 -0400

    SKFP-227 update nubmer of shards for variant_centric

--- bin/run_emr_job_indexer.sh
+++ bin/run_emr_job_indexer.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-index="variants_suggestions"
+index="variant_centric"
 release_id=${1:-"re_000017"}
 input=${2:-"s3a://kf-strides-variant-parquet-prd/portal/es_index/${index}_${release_id}/"}
 es_nodes=${3:-"https://vpc-kf-arranger-blue-es-service-exwupkrf4dyupg24dnfmvzcwri.us-east-1.es.amazonaws.com:443"}
 #es_nodes=${3:-"https://vpc-kf-arranger-blue-es-prd-4gbc2zkvm5uttysiqkcbzwxqeu.us-east-1.es.amazonaws.com:443"}
-es_index_name=${4:-"${index}"}
+es_index_name=${4:-"${index}_small_shards"}
 es_index_template=${5:-"${index}_template.json"}
 es_job_type=${6:-"index"} # one of: index, update, upsert or create
 es_batch_size=${7:-"500"} #default is 1000
-chromosome=${8:-"all"} #all, 1, 2, 3, ..., X, Y
+chromosome=${8:-"10"} #all, 1, 2, 3, ..., X, Y
 jarV=${9:-"7.12.0"}
 number_instance=${10:-"2"}
 instance_type=${11:-"r5.2xlarge"}
