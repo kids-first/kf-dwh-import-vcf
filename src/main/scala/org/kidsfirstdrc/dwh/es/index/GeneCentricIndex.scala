c002ce38ae1aed7d3eddf184c3283f46f7c09eca
evans-g-crsj <eg.crsj@gmail.com>
2023-05-17 14:57:04 -0400

    :sparkles: Add search_text field in gene_centric index

--- src/main/scala/org/kidsfirstdrc/dwh/es/index/GeneCentricIndex.scala
+++ src/main/scala/org/kidsfirstdrc/dwh/es/index/GeneCentricIndex.scala
@@ -2,36 +2,50 @@ package org.kidsfirstdrc.dwh.es.index
 
 import bio.ferlab.datalake.commons.config.{Configuration, RunType}
 import bio.ferlab.datalake.spark3.etl.ETL
-import org.apache.spark.sql.functions.{col, sha1}
+import org.apache.spark.sql.functions.{col, sha1, array, filter, array_union, when}
 import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 import org.kidsfirstdrc.dwh.conf.Catalog.{Es, Public}
 
 import java.time.LocalDateTime
 
-class GeneCentricIndex(releaseId: String)
-                      (override implicit val conf: Configuration) extends ETL() {
+class GeneCentricIndex(releaseId: String)(override implicit val conf: Configuration) extends ETL() {
 
   val destination = Es.gene_centric
 
-  override def extract(lastRunDateTime: LocalDateTime = minDateTime,
-                       currentRunDateTime: LocalDateTime = LocalDateTime.now())(implicit spark: SparkSession): Map[String, DataFrame] = {
+  override def extract(
+      lastRunDateTime: LocalDateTime = minDateTime,
+      currentRunDateTime: LocalDateTime = LocalDateTime.now()
+  )(implicit spark: SparkSession): Map[String, DataFrame] = {
     Map(
       Public.genes.id -> spark.table(s"${Public.genes.table.get.fullName}")
     )
   }
 
-  override def transform(data: Map[String, DataFrame],
-                         lastRunDateTime: LocalDateTime = minDateTime,
-                         currentRunDateTime: LocalDateTime = LocalDateTime.now())(implicit spark: SparkSession): DataFrame = {
+  override def transform(
+      data: Map[String, DataFrame],
+      lastRunDateTime: LocalDateTime = minDateTime,
+      currentRunDateTime: LocalDateTime = LocalDateTime.now()
+  )(implicit spark: SparkSession): DataFrame = {
     data(Public.genes.id)
       .withColumn("hash", sha1(col("symbol")))
+      .withColumn(
+        "search_text",
+        filter(
+          array_union(
+            array(col("symbol"), col("ensembl_gene_id")),
+            when(col("alias").isNotNull, col("alias")).otherwise(array().cast("array<string>"))
+          ),
+          x => x.isNotNull && x =!= ""
+        )
+      )
   }
 
-  override def load(data: DataFrame,
-                    lastRunDateTime: LocalDateTime = minDateTime,
-                    currentRunDateTime: LocalDateTime = LocalDateTime.now())(implicit spark: SparkSession): DataFrame = {
-    data
-      .write
+  override def load(
+      data: DataFrame,
+      lastRunDateTime: LocalDateTime = minDateTime,
+      currentRunDateTime: LocalDateTime = LocalDateTime.now()
+  )(implicit spark: SparkSession): DataFrame = {
+    data.write
       .mode(SaveMode.Overwrite)
       .option("format", destination.format.sparkFormat)
       .option("path", s"${destination.location}_$releaseId")
--- src/test/scala/org/kidsfirstdrc/dwh/es/index/GeneCentricIndexSpec.scala
+++ src/test/scala/org/kidsfirstdrc/dwh/es/index/GeneCentricIndexSpec.scala
@@ -10,24 +10,46 @@ import org.scalatest.GivenWhenThen
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class GeneCentricIndexSpec
-    extends AnyFlatSpec with GivenWhenThen with WithSparkSession with Matchers {
+class GeneCentricIndexSpec extends AnyFlatSpec with GivenWhenThen with WithSparkSession with Matchers {
   import spark.implicits._
 
-  val genesDf: DataFrame = Seq(
-    GenesOutput()
-  ).toDF()
-
-  val data = Map(
-    Public.genes.id -> genesDf
+  def makeDataMap(g: GenesOutput): Map[String, DataFrame] = Map(
+    Public.genes.id -> Seq(
+      g
+    ).toDF()
   )
 
   implicit val conf: Configuration = Configuration(List())
 
   "Gene_centric index job" should "transform data to the right format" in {
 
-    val result = new GeneCentricIndex("").transform(data)
+    val result = new GeneCentricIndex("").transform(makeDataMap(GenesOutput()))
     result.columns should contain allElementsOf Seq("hash")
     result.as[GeneCentricOutput].collect() should contain allElementsOf Seq(GeneCentricOutput())
+  }
+
+  "Gene_centric index job" should "create a search_text column" in {
+    // all
+    val g0 = GenesOutput(`symbol` = "s0", `alias` = List("a01"), `ensembl_gene_id` = "egi0")
+    val r0 = new GeneCentricIndex("").transform(makeDataMap(g0))
+    r0.columns should contain allElementsOf Seq("search_text")
+    r0.select("search_text").as[List[String]].collect() should contain theSameElementsAs Seq(Seq("s0", "egi0", "a01"))
+
+    // no ensembl_gene_id
+    val g1 = GenesOutput(`symbol` = "s1", `alias` = List("a11", "a12"), `ensembl_gene_id` = null)
+    val r1 = new GeneCentricIndex("").transform(makeDataMap(g1))
+    r1.columns should contain allElementsOf Seq("search_text")
+    r1.select("search_text").as[List[String]].collect() should contain theSameElementsAs Seq(Seq("s1", "a11", "a12"))
+
+    // no alias
+    val g2 = GenesOutput(`symbol` = "s2", `alias` = null, `ensembl_gene_id` = "egi2")
+    val r2 = new GeneCentricIndex("").transform(makeDataMap(g2))
+    r2.columns should contain allElementsOf Seq("search_text")
+    r2.select("search_text").as[List[String]].collect() should contain theSameElementsAs Seq(Seq("s2", "egi2"))
+
+    // edge case
+    val g3 = GenesOutput(`symbol` = null, `alias` = null, `ensembl_gene_id` = null)
+    val r3 = new GeneCentricIndex("").transform(makeDataMap(g3))
+    r3.select("search_text").as[List[String]].collect() shouldBe Seq(Seq())
   }
 }
